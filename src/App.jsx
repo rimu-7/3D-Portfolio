f69e6ae22c6002541266172727b5cc83b9ddbc1a
rimu-7 <rrimu99@gmail.com>
2025-04-17 14:27:43 +0800

    hero right side

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import Hero from './sections/Hero'
+import ShowCase from './sections/ShowCase'
+import Navbar from './sections/Navbar'
+import LogoSection from './sections/LogoSection'
+import FeatureCards from './sections/FeatureCards'
+import ExprienceSection from './sections/ExprienceSection'
 
 const App = () => {
   return (
     <>
+    <Navbar/>
     <Hero/>
+    <ShowCase/>
+    <LogoSection/>
+    <FeatureCards/>
+    <ExprienceSection/>
     </>
   )
 }
--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,10 +1,23 @@
 import React from "react";
 import { words } from "../constants";
 import Button from "../Components/Button";
+import HeroExpriences from "../Components/HeroModels/HeroExpriences";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import AnimatedCounter from "../Components/AnimatedCounter";
+
 
 const Hero = () => {
+  useGSAP(() => {
+    gsap.fromTo(
+      ".hero-text h1",
+      { y: 50, opacity: 0 },
+      { y: 0, opacity: 1, stagger: 0.2, duration: 1, ease: "power2.inOut" }
+    );
+  });
+
   return (
-    <section id="hero" className="relative overflow-hidden">
+    <section id="hero" className="relative overflow-hidden ">
       <div className="absolute top-0 left-0 z-10">
         <img src="/public/images/bg.png" alt="" />
       </div>
@@ -47,7 +60,13 @@ const Hero = () => {
           </div>
         </header>
         {/* right side */}
+        <figure>
+            <div className="hero-3d-layout">
+                <HeroExpriences/>
+            </div>
+        </figure>
       </div>
+      <AnimatedCounter/>
     </section>
   );
 };
